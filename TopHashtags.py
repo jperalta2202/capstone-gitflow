af96288c83ed67191daa46498cf5be4ccf7a70cf
jperalta2202 <jperalta@uc.cl>
2022-03-28 23:01:47 -0300

    top hashtags tweets

---
+++ TopHashtags.py
@@ -0,0 +1,25 @@
+
+import re
+
+def top_hashtags(data):
+
+
+    hashtags = {}
+
+    for tweet in data:
+
+        
+        tweet_hashtags = re.findall(r"#(\w+)", tweet['renderedContent'])
+
+        for hashtag in tweet_hashtags:
+
+            if hashtag in hashtags:
+                hashtags[hashtag] += 1
+            else:
+                hashtags[hashtag] = 1
+
+    result = sorted(hashtags.items(), key=lambda item: item[1], reverse=True)
+
+    print('--------Top 10 Users With Most Tweets--------')
+    for i in range(0, 9):
+        print("Hashtag: ", result[i][0], "Tweet Count: ", result[i][1]) 
