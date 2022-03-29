9c76d8d52fa364a3112947d83d9b56071af4721c
jperalta2202 <jperalta@uc.cl>
2022-03-28 21:39:34 -0300

    top 10 users tweets

---
+++ TopTweetsUsers.py
@@ -0,0 +1,21 @@
+
+import re
+
+
+def top_tweets_users(data):
+
+
+    users = {}
+
+    for tweet in data:
+
+        if tweet['user']['id'] in users:
+            users[tweet['user']['id']] += 1
+        else:
+            users[tweet['user']['id']] = 1
+
+    result = sorted(users.items(), key=lambda item: item[1], reverse=True)
+
+    print('--------Top 10 Users With Most Tweets--------')
+    for i in range(0, 9):
+        print("User Id: ", result[i][0], "Tweet Count: ", result[i][1]) 
